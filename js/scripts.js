b71b5774c392cb0d76ef7f27c07f7538a22bf6c3
Katarzyna Knapik <knapikkasia@gmail.com>
2018-06-20 18:30:41 +0200

    Zadanie 9.1

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+function getTriangleArea(a, h) {
+  if (a > 0 && h > 0);
+  else {
+    console.log('Nieprawidłowe dane')
+  } {
+    return a * h / 2;
+  }
+}
+console.log(getTriangleArea(10, 6))
+var triangle1Area = getTriangleArea(10, 15),
+    triangle2Area = getTriangleArea(150, 150),
+    triangle3Area = getTriangleArea(3, 6);
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
