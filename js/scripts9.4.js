72c9c7c197f35015c05a7b34baead2efca17ffe0
Katarzyna Knapik <knapikkasia@gmail.com>
2018-06-21 13:08:33 +0200

    Update zadanie 9.4

--- js/scripts9.4.js
+++ js/scripts9.4.js
@@ -1,10 +1,13 @@
-var star = ""
 
-function drawTree() {
-  for (var i = 0; i < 10; i++) {
-    for (var j = i; j = i; j++) {
-      star += "*";
-      console.log(star);
+
+function drawTree(height) {
+  for (var i = 0; i < height; i++) {
+  	var star = "";
+    for (var j = i; j >= 0; j--) {
+      star += " * ";
+      
     }
+    console.log(star);
   }
 }
+drawTree(10)
