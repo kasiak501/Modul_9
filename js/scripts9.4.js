b20686154f1481528e9181dfafc60f795aed1da3
Katarzyna Knapik <knapikkasia@gmail.com>
2018-06-21 15:14:45 +0200

    Add styling zadanie 9.4

--- js/scripts9.4.js
+++ js/scripts9.4.js
@@ -2,12 +2,11 @@
 
 function drawTree(height) {
   for (var i = 0; i < height; i++) {
-  	var star = "";
+    var star = "";
     for (var j = i; j >= 0; j--) {
       star += " * ";
-      
     }
     console.log(star);
   }
 }
-drawTree(10)
+drawTree(10);
