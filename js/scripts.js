5491c03c3af2b7375c9fd93bba6d20852e97c407
Katarzyna Knapik <knapikkasia@gmail.com>
2018-06-21 12:14:44 +0200

    Update Zadanie 9.1

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,14 @@
 function getTriangleArea(a, h) {
-  if (a > 0 && h > 0);
-  else {
-    console.log('Nieprawidłowe dane')
-  } {
-    return a * h / 2;
-  }
+  if (a > 0 && h > 0) {
+  	return a * h / 2;
+  } else {
+    return 'Nieprawidłowe dane';
+  } 
 }
 console.log(getTriangleArea(10, 6))
 var triangle1Area = getTriangleArea(10, 15),
     triangle2Area = getTriangleArea(150, 150),
-    triangle3Area = getTriangleArea(3, 6);
+    triangle3Area = getTriangleArea(-3, 6);
 console.log(triangle1Area);
 console.log(triangle2Area);
 console.log(triangle3Area);
